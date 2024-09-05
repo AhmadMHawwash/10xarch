55b91b39fe331a40d8173c01b4fd2e115687e32d
Hawwash <ahmad.hawwash@fyndiq.com>
2024-09-05 07:51:03 +0200

    trigger build

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -46,6 +46,7 @@ export async function updateSession(request: NextRequest) {
     const url = request.nextUrl.clone();
     url.pathname = "/signin";
     return NextResponse.redirect(url);
+    
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're

d0a03c3d074c551d5677b02b07ad42381ee738c2
Hawwash <ahmad.hawwash@fyndiq.com>
2024-09-05 07:56:41 +0200

    fix DATABASE_URL env var

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL ?? "",
   },
   tablesFilter: ["system-design-playground_*"],
 } satisfies Config;

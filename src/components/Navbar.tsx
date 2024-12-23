71a88136fc78951b0fa62a52df0b92f6e16eb31d
Hawwash <ahmad.hawwash@fyndiq.com>
2024-12-23 20:58:06 +0100

    some nav bar ui updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,27 +13,30 @@ export default function Navbar() {
   return (
     <nav className="h-[7vh] bg-slate-100 p-4 shadow-md dark:bg-gray-800">
       <div className="mx-auto flex items-center justify-between">
-        <Link
-          href="/"
-          className="text-xl font-bold text-gray-900 dark:text-white flex gap-2"
-        >
-          <Logo className="h-8 w-8 pt-0.5 pb-0.5" />
-          Archround
-        </Link>
+        <span className="flex items-center space-x-4">
+          <Link
+            href="/"
+            className="flex gap-2 text-xl font-bold text-gray-900 dark:text-white"
+          >
+            <Logo className="h-8 w-8 pb-0.5 pt-0.5" />
+            <span className="hidden md:block">Archround</span>
+          </Link>
+          <span className="flex items-center space-x-4 border-l border-gray-400 pl-4">
+            {[
+              { name: "Challenges", href: "/challenges" },
+              { name: "Playground", href: "/playground" },
+            ].map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white underline-offset-4 hover:underline transition-colors duration-200"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </span>
+        </span>
         <div className="hidden items-center space-x-4 md:!flex">
-          <Link
-            href="/challenges"
-            className="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
-          >
-            Challenges
-          </Link>
-          <Link
-            href="/playground"
-            className="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
-          >
-            Playground
-          </Link>
-
           <SignedOut>
             <SignInButton />
           </SignedOut>

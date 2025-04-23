14acc82f54a2de400f6a130b62c64d7492c1b239
Hawwash <ahmad.hawwash@fyndiq.com>
2025-04-23 09:26:44 +0200

    Refactor TRPCReactProvider to use httpBatchLink instead of unstable_httpBatchStreamLink for improved stability and performance

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import { useState } from "react";
 import SuperJSON from "superjson";
@@ -48,7 +48,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             process.env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
+        httpBatchLink({
           transformer: SuperJSON,
           url: getBaseUrl() + "/api/trpc",
           headers: () => {
